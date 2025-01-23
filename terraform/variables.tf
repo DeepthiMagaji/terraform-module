2861672f8f695d785707021af02d2b43b06b095b
DeepthiMagaji <deepthi.m2016@gmail.com>
2025-01-23 17:18:50 +0100

    type added for tflint

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "ami" {
   description = "ami for our instance"
+  type = string
 }
 
 variable "instance_type" {
   description = "the instance type of our instance"
+  type = string
 }
 
 #variable "sg" {
@@ -12,10 +14,12 @@ variable "instance_type" {
 
 variable "tag_name" {
   description = "name of the tag"
+  type = string
 }
 
 variable "user_data" {
   description = "userdata that will install webserver bash script"
+  type = string
 }
 
 #variable "iam_instance_profile" {
@@ -23,48 +27,50 @@ variable "user_data" {
 #}
 
 variable "role_name" {
-
+  type = string
 }
 
 variable "policy_name" {
-
+  type = string
 }
 
 variable "instance_profile_name" {
-
+  type = string
 }
 
 variable "path" {
-
+  type = string
 }
 
 variable "iam_policy" {
-
+  type = string
 
 }
 
 variable "assume_role_policy" {
-
+  type = string
 }
 
 variable "iam_policy_description" {
-
+  type = string
 }
 
 variable "bucket_name" {
-
+  type = string
 }
 
 variable "acl" {
-
+  type = string
 }
 
 variable "object_key" {
-
+  type = string
 }
 
 variable "object_source" {
-
+  type = string
 }
 
-variable "region" {}
+variable "region" {
+  type = string
+}
