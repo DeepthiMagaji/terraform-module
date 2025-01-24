a0abcb8dbc26cfa0cc79b92b9caa06225fadb506
DeepthiMagaji <deepthi.m2016@gmail.com>
2025-01-24 15:26:16 +0100

    made changes to s3 module

--- test/s3_test.go
+++ test/s3_test.go
@@ -49,6 +49,11 @@ func TestS3Module(t *testing.T) {
    		t.Fatalf("Expected object key to be %s but got %s", config.ObjectKey, outputObjectKey)
    	}
 
+   	outputBucketName := terraform.Output(t, terraformOptions, "object_source")
+        if outputBucketName != config.ObjectSource {
+            t.Fatalf("Expected bucket name to be %s but got %s", config.ObjectSource, outputObjectSource)
+        }
+
    	// Ensure cleanup works by checking that the bucket was destroyed
    	if terraform.Output(t, terraformOptions, "bucket_name") != "" {
    	    t.Fatalf("Bucket was not destroyed successfully. It still exists!")
